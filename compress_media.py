afdccca2c24c0d53c58129375bef876b12045a3d
Remon Felix <remonfelix97@gmail.com>
2026-01-23 19:51:40 +0100

    fixed missing images, minor things

--- compress_media.py
+++
@@ -1,7 +0,0 @@
-
-def compress_image(input_path, output_path, filesize_threshold=500):
-    """Function to compress an image using ffmpeg to a maximum of the specified filesize threshold [kB]. 
-    It will compress in multiple stages if necessary to meet the threshold."""
-
-    for image in r"D:\Code\Project_Van_Hool\Media":
-        
